13a4d329677d06bdd7e3f25b21bec405561f2d77
Dashulikdv <102612960+Dashulikdv@users.noreply.github.com>
2022-03-29 20:19:11 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
 public class Main{
   public static void main(String[] args){
-    int a=20;
-    int b=30;
-    int c=34;
-    int d=12;
-    int max=d;
-    if(a>b&& a>c@@ a>d){
-      max=a;
-    } else if( b>c && b>
-    System.out.printl("Max= " +(a>b?a:b));
-}
+    int number=265434;
+    while(number>9){
+      int d1=number%10;
+      number/=10;
+      if(d1!=d2)
+        return false;
+    }
+    return true;
+    
+  }
